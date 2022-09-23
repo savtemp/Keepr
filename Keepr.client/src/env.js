7fa1277fd8975814225521d48ae4320cfa947485
Savannah Temple <savtemple@gmail.com>
2022-09-23 10:47:29 -0600

    init commit

---
+++ Keepr.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes("localhost");
+export const baseURL = dev ? "https://localhost:5001" : "";
+export const useSockets = false;
+export const domain = "dev-gmunio0i.us.auth0.com";
+export const clientId = "I6v1O4PjfTtnUidIlC7MPEUr8KGKITnl";
+export const audience = "https://codeworksclassrom.com";
